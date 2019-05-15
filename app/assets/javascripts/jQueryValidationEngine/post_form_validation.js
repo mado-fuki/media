3e602894afab41d4e12a774e5e16484796965158
mado-fuki <nkutl9up@gmail.com>
2019-05-15 21:29:20 +0900

    add user form description

--- app/assets/javascripts/jQueryValidationEngine/post_form_validation.js
+++
@@ -1,3 +0,0 @@
-// jQuery(document).ready(function($){
-//   jQuery("#new_post").validationEngine();
-// });
--- app/assets/javascripts/jQueryValidationEngine/user_form_validation.js
+++ app/assets/javascripts/jQueryValidationEngine/user_form_validation.js
@@ -1,4 +1,5 @@
 jQuery(document).ready(function($){
   jQuery("#session_form").validationEngine();
   jQuery("#user_form").validationEngine();
+  jQuery("#password_reset_form").validationEngine();
 });
