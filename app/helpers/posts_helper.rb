ccad7af4858d68904bc80aa97ee254bda4871653
mado-fuki <nkutl9up@gmail.com>
2019-07-21 15:34:08 +0900

    display-Search-keyword

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -2,4 +2,16 @@ module PostsHelper
   def create_tag_link(post)
     raw post.tag_list.map { |t| link_to "#"+t, tag_path(t) }.join(', ')
   end
+
+  def get_search_word(words)
+    unless words.blank?
+      "キーワード検索: " + words
+    end
+  end
+
+  def get_search_tag
+    if request.fullpath.include?("/tags/")
+      "タグ検索: " + URI.decode(request.fullpath).sub("/tags/", "")
+    end
+  end
 end
